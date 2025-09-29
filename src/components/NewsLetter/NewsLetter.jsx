b310688de2caadd5f7c1cd9c9f15c3924983ece3
OfriG <ofri900@gmail.com>
2025-09-29 13:04:07 +0300

    not final

---
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+async function getNewsLetterData() {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337'}/api/footer?populate[logo][populate]=*&populate[power_logo][populate]=*&populate[left_menu][populate]=*&populate[right_menu][populate]=*&populate[newsletter][populate]=*`,
+        { cache: "no-store" }
+    );
+    const data = await response.json();
+    return data.data;
+}
+
+export default function NewsLetter() {
+    const [newsLetter, setNewsLetter] = useState(null);
+
+    useEffect(() => {
+        getNewsLetterData().then(setNewsLetter);
+    }, []);
+
+    if (!newsLetter) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="newsletter">
+            <div className="newsletter_header">{newsLetter.newsletter?.header}</div>
+            <div className="newsletter_description">{newsLetter.newsletter?.description}</div>
+            <div className="newsletter_email">{newsLetter.newsletter?.email}</div>
+
+            <div className="newsletter_button">{newsLetter.newsletter?.button.text} </div>
+            <div className="newsletter_line">_</div>
+            <div className="newsletter_menu">
+            <div className="newsletter_left_menu">
+                <ul>
+                    {newsLetter.left_menu?.map((item) => (
+                        <li key={item.id}>
+                            <a href={`/${item.url}`}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+
+            <div>
+                <ul>
+                    {newsLetter.right_menu?.map((item) => (
+                        <li key={item.id}>
+                            <a href={`/${item.url}`}>{item.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div></div>
+    );
+}
